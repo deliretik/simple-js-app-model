6f9d9b991022e214fb143f0849dc7bd96ac2a8ad
deliretik <deliretik@gmail.com>
2021-10-20 12:48:00 +0200

    added modal function

--- js/scripts.js
+++ js/scripts.js
@@ -52,13 +52,62 @@ let pokemonRepository = (function () {
         console.error(e);
       });
     }
+//insert the modal function
 
-
+        let modalContainer = document.querySelector('#modal-container');
+        function showModal(item, loadDetails) {
+          modalContainer.innerHTML = '';
+          let modal = document.createElement('div');
+          modal.classList.add('modal');
+      
+          let closeButtonElement = document.createElement('button');
+          closeButtonElement.classList.add('modal-close');
+          closeButtonElement.innerText = 'Close';
+          closeButtonElement.addEventListener('click', hideModal);
+      
+          let titleElement = document.createElement('h1');
+          titleElement.innerText = item;
+      
+          let contentElement = document.createElement('p');
+          contentElement.innerText = loadDetails(item);
+      
+          modal.appendChild(closeButtonElement);
+          modal.appendChild(titleElement);
+          modal.appendChild(contentElement);
+          modalContainer.appendChild(modal);
+      
+      
+          modalContainer.classList.add('is-visible');
+        }
+      
+        function hideModal() {
+          modalContainer.classList.remove('is-visible');
+        }
+      
+        window.addEventListener('keydown', (e) => {
+          if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();  
+          }
+        });
+        modalContainer.addEventListener('click', (e) => {
+          // Since this is also triggered when clicking INSIDE the modal
+          // We only want to close if the user clicks directly on the overlay
+          let target = e.target;
+          if (target === modalContainer) {
+            hideModal();
+          }
+        });
+      
+        document.querySelector('#show-modal').addEventListener('click', () => {
+          showModal('Modal title', 'This is the modal content!');
+        });
+//modal function end
 
 
     function showDetails(pokemon) {
       loadDetails(pokemon).then(function () {
-        console.log(pokemon);
+        //console.log(pokemon);
+        showModal();
       });
     }
     
